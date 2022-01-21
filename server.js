db868f9ae28249d9f48ad5daccaae5b42041cced
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2022-01-21 21:39:46 +0500

    'initial'

--- server.js
+++ server.js
@@ -1,37 +1,40 @@
-const express=require('express')
-const connectDb=require('./config/db') 
-const dotenv =require('dotenv')
-const error=require('./middlewares/errorMiddleware')
-const colors =require('colors')
-const {products}=require('./data/products')
+const express = require("express");
+const connectDb = require("./config/db");
+const dotenv = require("dotenv");
+const error = require("./middlewares/errorMiddleware");
+const colors = require("colors");
+const { products } = require("./data/products");
 
 // Product Routes
-const productRoutes=require('./routes/productRoutes')
+const productRoutes = require("./routes/productRoutes");
 // User Routes
-const userRoutes=require('./routes/userRoutes')
+const userRoutes = require("./routes/userRoutes");
 // Order Routes
-const orderRoutes=require('./routes/orderRoutes')
+const orderRoutes = require("./routes/orderRoutes");
 
-dotenv.config()
+dotenv.config();
 
-connectDb()
+connectDb();
 
-const app=express()
- 
-app.use(express.json())
+const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("App is running...")
-})
+app.use(express.json());
 
-app.use('/api/products',productRoutes)
+app.get("/", (req, res) => {
+  res.send("App is running...");
+});
 
-app.use('/api/users',userRoutes)
+app.use("/api/products", productRoutes);
 
-app.use('/api/orders',orderRoutes)
+app.use("/api/users", userRoutes);
 
-app.use(error.notFound)
+app.use("/api/orders", orderRoutes);
 
-app.use(error.errorHandler)
+app.use(error.notFound);
 
-app.listen(5000,console.log('Server running on port 5000'))
+app.use(error.errorHandler);
+console.log("dotenv", process.env);
+app.listen(
+  process.env.PORT || 5000,
+  console.log("Server running on port 5000")
+);
